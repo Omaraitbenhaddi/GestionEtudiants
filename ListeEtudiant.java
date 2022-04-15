f2cb2069aa17c9bd7d4c782941d56ad56c3c0758
Omaraitbenhaddi <88466716+Omaraitbenhaddi@users.noreply.github.com>
2022-04-15 13:37:30 +0200

    methode add

--- ListeEtudiant.java
+++ ListeEtudiant.java
@@ -1,5 +1,13 @@
 import java.util.ArrayList;
 
-public class ListeEtudiant extends ArrayList<Etudiant> {
+public class ListeEtudiant extends ArrayList<Etudiant>{
+    @Override
+    public boolean add(Etudiant e) {
+        if(this.contains(e)){
+            this.get(this.indexOf(e)).nom=e.nom;
+            this.get(this.indexOf(e)).note=e.note;
+        }
+        return super.add(e);
+    }
     
 }
