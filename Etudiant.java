f3dc18de8433ac6902d0300f929eda2372617fe2
Omaraitbenhaddi <88466716+Omaraitbenhaddi@users.noreply.github.com>
2022-04-15 11:43:25 +0200

    first commit

---
+++ Etudiant.java
@@ -0,0 +1,20 @@
+public class Etudiant{
+    int id;
+    String nom;
+    double note;
+    public Etudiant(int id, String nom) {
+        this.id = id;
+        this.nom = nom;
+        this.note=10.0;
+    }
+    @Override
+    public String toString() {
+        return "(" + nom + " : " + note + ")";
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if(!(obj instanceof Etudiant)) return false;
+        return ((Etudiant)obj).id==id;
+    }
+    
+}
