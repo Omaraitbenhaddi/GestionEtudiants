d7d65f54b42beb09f55b7f74d07f443690303a17
Omaraitbenhaddi <88466716+Omaraitbenhaddi@users.noreply.github.com>
2022-04-15 14:01:50 +0200

    filtrer travaille partielle

--- ListeEtudiant.java
+++ ListeEtudiant.java
@@ -1,10 +1,21 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class ListeEtudiant extends ArrayList<Etudiant>{
     @Override
     public boolean add(Etudiant e) {
         if(this.contains(e)) this.set(this.indexOf(e),e);
         return super.add(e);
+    }
+    public List<Etudiant> filtrer(Condition c){
+        List<Etudiant> l = new ListeEtudiant();
+        for(Etudiant elem:this){
+            if(c.estVrai(elem)){
+                l.add(elem);
+            }
+
+        }
+        return l;
     }
     
     
